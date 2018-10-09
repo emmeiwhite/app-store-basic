f81c4641ff7730dab4a0c3be0e4fcb4504ff453f
emmeiwhite <emmeiwhite@gmail.com>
2018-10-09 16:44:28 +0530

    deleting the Components

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        value:0
+        value:this.props.value // from parent props is being passed 
     }
 
     getbadgeClass = ()=>{
@@ -13,13 +13,16 @@ class Counter extends Component {
     }
 
     handleIncrement = ()=>{
+        // this.props.value = 23; props are read only and cannot be modified
         this.setState({value:this.state.value+1});
     }
+
     render() { 
         return ( 
             <div className="counter">
                 <span className={this.getbadgeClass()+' value'}>{this.state.value===0?"Zero":this.state.value}</span>
                 <button className="btn btn-primary btn-lg" onClick={this.handleIncrement}>Increment</button>
+                <button className="btn btn-danger  btn-sm m-2" onClick={this.props.onDelete}>Delete</button> {/* Idea:function was passed as prop*/}
             </div> 
         );
     }
--- src/components/counters.js
+++ src/components/counters.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Counter from './counter';
 
-class Counters extends Component {
+class Counters extends Component { 
+    //Parent feeds children on the children's demand.
     state = { 
         counters:[
             {id:1,value:0},
@@ -10,11 +11,18 @@ class Counters extends Component {
             {id:4,value:0},
         ]
      }
+
+    deleteHandler = (index)=>{
+     const newState =  {...this.state}; // copied the state
+     newState.counters.splice(index,1);
+     this.setState(newState);
+    }
+
     render() { 
         return ( 
             <div className="counters">
-             {this.state.counters.map((counter)=>{
-                 return <Counter key={counter.id}/> 
+             {this.state.counters.map((counter,index)=>{
+                 return <Counter key={counter.id} value={counter.value} onDelete={()=>this.deleteHandler(index)}/> 
              })}
             </div>
            
