2ad04b40bc67d9a3c11c9c6d3cd55d6be0ac9ee6
emmeiwhite <emmeiwhite@gmail.com>
2018-10-10 15:20:03 +0530

    Navbar created as a stateless functional Component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
-class Navbar extends Component {
-    render() { 
-        return (
+
+// Stateless Functional Component
+
+const Navbar = (props)=>{
+    return (
         <nav class="navbar navbar-light bg-light">
             <a class="navbar-brand" href="#">
-                Navbar <button className="btn btn-primary btn-sm">{this.props.totalCounters}</button>
+                Navbar <button className="btn btn-primary btn-sm">{props.totalCounters}</button>
             </a>
         </nav>
           );
-    }
 }
  
 export default Navbar;
