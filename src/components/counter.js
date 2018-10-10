9c98e0d3cfdf6945281c4947ff6bb8c5d597e6bb
emmeiwhite <emmeiwhite@gmail.com>
2018-10-10 12:24:11 +0530

    Controlled Component - No state of it's own, Depends on Parent for everything

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = { 
-        value:this.props.counter.value // from parent props is being passed 
-    }
+    // Removing local state from the child
+   
 
     getbadgeClass = ()=>{
         // Conditional Rendering
         let classes = 'btn btn-';
-        classes+= this.state.value===0?'warning':'danger';
+        classes+= this.props.counter.value===0?'warning':'danger';
         return classes;
     }
 
-    handleIncrement = ()=>{
-        // this.props.value = 23; props are read only and cannot be modified
-        this.setState({value:this.state.value+1});
+    formatCount = ()=>{
+        let {value} =  {...this.props.counter} // Destructuring: from counter object {id:--,value:--}, we are fetching hte value
+        console.log(value);
+        return value===0?"ZERO":value;
+     
     }
-
+    // handleIncrement will also come from the parent only: In short both data and implementation will be handled by Parent
     render() { 
         return ( 
             <div className="counter">
-                <span className={this.getbadgeClass()+' value'}>{this.state.value===0?"Zero":this.state.value}</span>
-                <button className="btn btn-primary btn-lg" onClick={this.handleIncrement}>Increment</button>
+                <span className={this.getbadgeClass()+' value'}>{this.formatCount()}</span>
+                <button className="btn btn-primary btn-lg" onClick={()=>this.props.onIncrement(this.props.counter)}>Increment</button>
                 <button className="btn btn-danger  btn-sm m-2" onClick={()=>this.props.onDelete(this.props.counter.id)}>Delete</button> {/* Idea:function was passed as prop*/}
             </div> 
         );
--- src/components/counters.js
+++ src/components/counters.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Counter from './counter';
-import { timingSafeEqual } from 'crypto';
 
 class Counters extends Component { 
     //Parent feeds children on the children's demand.
+    // WE MUST WORK WITH SINGLE SOURCE OF TRUTH TO MAKE SURE THE RESET WORKS
     state = { 
         counters:[
             {id:1,value:0},
@@ -13,7 +13,7 @@ class Counters extends Component {
         ]
      }
 
-    deleteHandler = (counterId)=>{
+    handleDelete = (counterId)=>{
         const counters = this.state.counters.filter(c=>c.id!==counterId);
         this.setState({counters})
     }
@@ -22,22 +22,34 @@ class Counters extends Component {
         // Let's try to modify the state by setting the value of counters to 0, let's check the bahavior
 
         const counters = this.state.counters.map(counter=>{
-            return counter.value=0;
+            counter.value=0;
+            return counter;
         });
 
         this.setState({counters}); 
+    }
+    
+    handleIncrement = (counter)=>{
+        const counters = [...this.state.counters];//copied the counters
+        // We just need to modify that counter on which user clicks. let's find the index of that counter
+        const index = counters.indexOf(counter);
+        counters[index] =  {...counter}; 
+        counters[index].value++;
 
+        this.setState({counters});
 
     }
+
     render() { 
         return ( 
             <div className="counters">
                 <button className="btn btn-primary m-2 btn-lg" onClick={this.handleReset}>Reset</button>
-             {this.state.counters.map((counter)=>{
+             {this.state.counters.map((counter)=>{ // particular counter {id:--,value:--} for particular Counter
                  return <Counter 
                         key={counter.id}
                         counter={counter}
-                        onDelete={this.deleteHandler}/> 
+                        onIncrement={this.handleIncrement}
+                        onDelete={this.handleDelete}/> 
               })}
             </div>
            
