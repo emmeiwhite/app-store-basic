7ac9e75149f3ac295a090f76eea9de1a12251562
emmeiwhite <emmeiwhite@gmail.com>
2018-10-10 13:44:46 +0530

    Navbar component Added in the App tree

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Counters from './components/counters';
 import './App.css';
 
-import Counters from './components/counters';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-          <Counters/>
+      <div className="row">
+          <Navbar/>
+          <Counters className="col-md-8"/>
       </div>
     );
   }
